751d0e96b8830d0f2a23d354c729d89d808a512a
sir-bravin111 <mrbravin69@gmail.com>
2025-11-14 18:15:11 +0300

    Update random.js

--- bravo/random.js
+++ bravo/random.js
@@ -6,11 +6,11 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-const adams = require(__dirname + "/../config");
+const bravo = require(__dirname + "/../config");
 
 async function fetchRANDOMUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(bravo.jeepees_creeper);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("RANDOM")');
